0417dfb21d2bd07e533381b592d8552a43610f5d
IgorMontezuma20 <igorminranda17@gmail.com>
2023-02-14 14:57:09 -0300

    mudanca no nome da RegisterFragment

--- app/src/main/java/com/app/ufit/ui/fragments/register/RegisterFragment.kt
+++ app/src/main/java/com/app/ufit/ui/fragments/register/RegisterFragment.kt
@@ -7,13 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.app.ufit.R
 
-class RegisterFragment2 : Fragment() {
+class RegisterFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register2, container, false)
+        return inflater.inflate(R.layout.fragment_register, container, false)
     }
 
 }
