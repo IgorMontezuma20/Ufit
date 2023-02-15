b380cf492835ee6a84f7b6988be3cc1514f82dc0
Renato <renato.rodrigues@foursys.com.br>
2023-02-15 12:04:02 -0300

    Renato - choose your goal

---
+++ app/src/main/java/com/app/ufit/ChooseYourGoal.kt
@@ -0,0 +1,11 @@
+package com.app.ufit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChooseYourGoal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chose_goal)
+    }
+}
