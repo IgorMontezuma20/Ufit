f0d887c8abdd4bb35df5f0a08ca529877d7075f1
IgorMontezuma20 <igorminranda17@gmail.com>
2023-02-15 11:39:35 -0300

    navegacoes implementadas

--- app/src/main/java/com/app/ufit/ui/fragments/register/RegisterFragment.kt
+++ app/src/main/java/com/app/ufit/ui/fragments/register/RegisterFragment.kt
@@ -5,15 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.app.ufit.R
+import com.app.ufit.databinding.FragmentRegisterBinding
+import com.app.ufit.databinding.FragmentRegisterInfoBinding
 
 class RegisterFragment : Fragment() {
+
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+
+        binding.button.setOnClickListener {
+            findNavController().navigate(R.id.action_registerFragment2_to_registerInfoFragment)
+        }
+
+        return binding.root
     }
 
 }
--- app/src/main/java/com/app/ufit/ui/fragments/register/RegisterInfoFragment.kt
+++ app/src/main/java/com/app/ufit/ui/fragments/register/RegisterInfoFragment.kt
@@ -6,16 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.app.ufit.R
+import com.app.ufit.databinding.FragmentLoginBinding
+import com.app.ufit.databinding.FragmentRegisterBinding
+import com.app.ufit.databinding.FragmentRegisterInfoBinding
 
 
 class RegisterInfoFragment : Fragment() {
 
+    private var _binding: FragmentRegisterInfoBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register_info, container, false)
+        _binding = FragmentRegisterInfoBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
 }
