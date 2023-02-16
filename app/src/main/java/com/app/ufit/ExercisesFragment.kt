142855dd7883b564c49819da39655854e0de670b
IgorMontezuma20 <igorminranda17@gmail.com>
2023-02-16 10:41:16 -0300

    mainViewModel criada

---
+++ app/src/main/java/com/app/ufit/ExercisesFragment.kt
@@ -0,0 +1,20 @@
+package com.app.ufit
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ExercisesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_exercises, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/com/app/ufit/adapters/ExercisesAndroid.kt
@@ -0,0 +1,23 @@
+package com.app.ufit.adapters
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ExercisesAdapter: RecyclerView.Adapter<ExercisesAdapter.MyViewHolder>(){
+
+    class MyViewHolder: RecyclerView.ViewHolder() {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
