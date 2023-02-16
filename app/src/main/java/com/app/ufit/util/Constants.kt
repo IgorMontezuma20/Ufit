0117f9094aef7705caa4cc34fe04d2a138316dad
IgorMontezuma20 <igorminranda17@gmail.com>
2023-02-15 21:57:11 -0300

    correcao na BASE_URL

--- app/src/main/java/com/app/ufit/util/Constants.kt
+++ app/src/main/java/com/app/ufit/util/Constants.kt
@@ -6,7 +6,7 @@ class Constants {
 
         //Api
         const val API_KEY = "08xv1H0Df5Gc3/cq5W+RvQ==857WO7r0RJFKs53N"
-        const val BASE_URL = "https://api-ninjas.com/api/exercises"
+        const val BASE_URL = "https://api-ninjas.com/api"
 
 
     }
