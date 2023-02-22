95ec1be196a4523f96151307f72e277d8839843a
Gabriel Farias <imgabrielsf@gmail.com>
2023-02-22 12:17:18 -0300

    onClickListener

---
+++ app/src/main/java/com/app/ufit/bindingadapters/MuscleGroupItemBinding.kt
@@ -0,0 +1,28 @@
+package com.app.ufit.bindingadapters
+
+import android.util.Log
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.databinding.BindingAdapter
+import androidx.navigation.findNavController
+import com.app.ufit.ui.fragments.home.HomeFragment
+import com.app.ufit.ui.fragments.home.HomeFragmentDirections
+
+class MuscleGroupItemBinding {
+
+    companion object {
+
+        @BindingAdapter("onRecipeClickListener")
+        @JvmStatic
+        fun onMuscleGroupClickListener(muscleGroupLayout: ConstraintLayout, muscle: String){
+            muscleGroupLayout.setOnClickListener{
+                try{
+                    val action =
+                        HomeFragmentDirections.actionHomeFragmentToExercisesFragment(muscle)
+                    muscleGroupLayout.findNavController().navigate(action)
+                }catch (e: Exception){
+                    Log.d( "onMuscleGroupClickListener", e.toString())
+                }
+            }
+        }
+    }
+}
