d6674cf4fbafaa185740c9d230860275de47e8a6
IgorMontezuma20 <igorminranda17@gmail.com>
2023-05-09 14:40:24 -0300

    criando funcionalidade de favoritar

---
+++ app/src/main/java/com/app/ufit/adapters/FavoriteAdapter.kt
@@ -0,0 +1,63 @@
+package com.app.ufit.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.app.ufit.R
+import com.app.ufit.data.database.FavoritesDao
+import com.app.ufit.data.database.entities.FavoritesEntity
+
+class FavoriteAdapter(
+    private val context: Context,
+    private val favoriteDao: FavoritesDao,
+    private val list: MutableList<FavoritesEntity>,
+    //private val deleteItemClickListener: DeleteItemClickListner
+) : RecyclerView.Adapter<FavoriteAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.exercise_item, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val model = list[position]
+
+        holder.title.text = model.name
+        holder.description.text = model.instructions
+        holder.muscle.text = model.muscle
+        holder.difficulty.text = model.difficulty
+
+
+
+        val isFavorite = favoriteDao.isFavorite(model.id)
+        if (isFavorite) {
+            holder.iv_favorite.setImageResource(R.drawable.ic_favorite)
+        } else {
+            holder.iv_favorite.setImageResource(R.drawable.ic_favorite_border)
+        }
+
+//        holder.iv_favorite.setOnClickListener {
+//            deleteItemClickListener.onItemDelete(position, list[position].id)
+//        }
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    interface DeleteItemClickListner {
+        fun onItemDelete(position: Int, id: Int)
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var title: TextView = view.findViewById(R.id.tvTitle)
+        var description: TextView = view.findViewById(R.id.tvDesc)
+        var muscle: TextView = view.findViewById(R.id.tvMuscle)
+        var difficulty: TextView = view.findViewById(R.id.tvDifficulty)
+        var iv_favorite: ImageView = view.findViewById(R.id.iv_favorite)
+    }
+}
--- app/src/main/java/com/app/ufit/ui/fragments/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/app/ufit/ui/fragments/favorites/FavoritesFragment.kt
@@ -6,13 +6,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
+import com.app.ufit.adapters.ExercisesAdapter
+import com.app.ufit.adapters.FavoriteAdapter
+import com.app.ufit.data.database.ExercisesDatabase
+import com.app.ufit.data.database.FavoritesDao
+import com.app.ufit.data.database.entities.FavoritesEntity
 import com.app.ufit.databinding.FragmentFavoritesBinding
 import com.app.ufit.viewmodels.exercise.ExerciseDetailsViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.internal.Contexts.getApplication
 
-
+@AndroidEntryPoint
 class FavoritesFragment : Fragment() {
 
     private lateinit var binding: FragmentFavoritesBinding
+
+    private lateinit var mAdapter: FavoriteAdapter
+    private lateinit var mFavoriteDao: FavoritesDao
+
+    private lateinit var exercisesDatabase: ExercisesDatabase
+    private lateinit var favoritesList: List<FavoritesEntity>
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,11 +38,41 @@ class FavoritesFragment : Fragment() {
         binding = FragmentFavoritesBinding.inflate(inflater, container, false)
 
 
+        setupRecyclerView()
+
+
         return binding.root
 
     }
 
+    override fun onResume() {
+        super.onResume()
+        val db = Room.databaseBuilder(
+            requireContext(),
+            ExercisesDatabase::class.java, "ExercisesDatabase"
+        ).build()
+        mFavoriteDao = db.favoritesDao()
+
+        mAdapter = FavoriteAdapter(requireContext(), mFavoriteDao, mutableListOf())
+        binding.rvFavoriteExercises.adapter = mAdapter
+        binding.rvFavoriteExercises.layoutManager = LinearLayoutManager(requireContext())
+
+        favoritesList = mFavoriteDao.getAllData()}
 
 
+    private fun setupRecyclerView() {
+        binding.rvFavoriteExercises.adapter = mAdapter
+        binding.rvFavoriteExercises.layoutManager = LinearLayoutManager(requireActivity(), LinearLayoutManager.VERTICAL, false)
+    }
+
+//    private fun getData() {
+//        val list = FavoritesEntity.getDatabase(getApplication()).dao().allData
+//        recyclerView.layoutManager = LinearLayoutManager(this)
+//        recyclerView.adapter =
+//            FavoriteAdapter(applicationContext, mFavoriteDao, list) { position, id ->
+//                FavoriteDatabase.getDatabase(applicationContext).dao().deleteData(id)
+//                getData()
+//            }
+//    }
 
 }
